dda1a922f935e0d63af2bb39e9f502c115d2f20d
Mitch Ryan <mitch@diesdas.digital>
2020-11-01 13:47:10 +0100

    Add "commander" dependency and add put current functionality in the "list" command

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,15 @@
 #!/usr/bin/env node
 
 import { Yeelight } from "ts-node-yeelight";
-
+import { Command } from "commander";
+const program = new Command();
 const yeelight = new Yeelight();
 
-yeelight.discover().then(() => {
-  console.log(yeelight.devices);
-});
+program
+  .command("list")
+  .description("Show the connected devices")
+  .action(() =>
+    yeelight.discover().then(() => {
+      console.log(yeelight.devices);
+    })
+  );
