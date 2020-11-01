de5890d7fb3f0d9d6086fe24928aaa187fa1a032
Mitch Ryan <mitch@diesdas.digital>
2020-11-01 14:51:09 +0100

    Add an "off" command

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,12 @@
 #!/usr/bin/env node
 
 import { YeelightService } from "yeelight-service";
+import {
+  IYeelight,
+  IYeelightDevice,
+  IYeelightMethodResponse,
+  YeelightMethodStatusEnum,
+} from "yeelight-service/lib/yeelight.interface";
 const yeelightService = new YeelightService();
 import { Command } from "commander";
 const program = new Command();
@@ -9,13 +15,51 @@ console.info(
   "⚠️  Make sure you enabled the LAN Control option in the Yeelight app."
 );
 
+async function getDevices(): Promise<IYeelightDevice[]> {
+  let devices: IYeelightDevice[] = [];
+
+  const subscriber = yeelightService.devices.subscribe((foundDevices) => {
+    devices = foundDevices;
+  });
+
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+
+  subscriber.unsubscribe();
+
+  return devices;
+}
+
 program
   .command("list")
   .description("Show the connected devices")
-  .action(() => {
-    yeelightService.devices.subscribe((devices) => {
-      console.log(devices.map((device) => device.id));
-    });
+  .action(async () => {
+    const devices = await getDevices();
+
+    console.log(devices.map((device) => device.id));
   });
 
-program.parse(process.argv);
+program
+  .command("off")
+  .description("Turn off all connected devices")
+  .action(async () => {
+    const devices = await getDevices();
+
+    console.log(`Turning off ${devices.length} devices.`);
+
+    const responses = await Promise.all(
+      devices.map((device) => device.setPower("off", "smooth"))
+    );
+
+    if (responses.every((response) => response.status === 200)) {
+      console.log("Done with no errors");
+      return;
+    }
+
+    const errorMessages = responses
+      .map((response) => response.errorMessage)
+      .filter(Boolean);
+
+    console.log(`Done with errors:\n${errorMessages.join("/n")}`);
+  });
+
+program.parseAsync(process.argv);
