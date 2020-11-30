356c27738351a6ff404c09de8da49471ae025177
Mitch Ryan <mitch@diesdas.digital>
2020-11-30 19:54:03 +0100

    Add a command to choose from presets

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,8 @@ import {
 import { Command } from "commander";
 import chalk from "chalk";
 import { filter, first, timeout, retry } from "rxjs/operators";
+import inquirer from "inquirer";
+import { rgb } from "polished";
 
 const yeelightService = new YeelightService();
 const program = new Command();
@@ -112,6 +114,71 @@ program
 
     console.log(chalk.red(`Done with errors:\n${errorMessages.join("\n")}`));
 
+    process.exit(2);
+  });
+
+program
+  .command("preset")
+  .description("Set devices to the chosen preset")
+  .action(async () => {
+    const { preset } = await inquirer.prompt<{
+      preset: { rgb: string; brightness: number };
+    }>([
+      {
+        name: "preset",
+        type: "list",
+        message: "Choose preset",
+        choices: [
+          // https://andi-siess.de/rgb-to-color-temperature/
+          {
+            name: "6500 - Neutral",
+            value: { rgb: rgb(255, 249, 253), brightness: 100 },
+          },
+          {
+            name: "4200 - Neutral",
+            value: { rgb: rgb(255, 213, 173), brightness: 100 },
+          },
+          {
+            name: "2900 - Warm",
+            value: { rgb: rgb(255, 177, 101), brightness: 100 },
+          },
+          {
+            name: "1700K - Warm",
+            value: { rgb: rgb(255, 121, 0), brightness: 100 },
+          },
+          {
+            name: "1000 – Neutral",
+            value: { rgb: rgb(255, 56, 0), brightness: 100 },
+          },
+        ],
+      },
+    ]);
+
+    const devices = await getDevices();
+
+    console.log(`Updating ${devices.length} devices…`);
+
+    const responses = await Promise.all(
+      devices
+        .map((device): Promise<IYeelightMethodResponse>[] => {
+          return [
+            attemptDeviceCommand(() => device.setRgb(preset.rgb)),
+            attemptDeviceCommand(() => device.setBrightness(preset.brightness)),
+          ];
+        })
+        .flat()
+    );
+
+    if (responses.every((response) => response.status === 200)) {
+      console.log(chalk.green("Done with no errors"));
+      process.exit(1);
+    }
+
+    const errorMessages = responses
+      .map((response) => response.errorMessage)
+      .filter(Boolean);
+
+    console.log(chalk.red(`Done with errors:\n${errorMessages.join("\n")}`));
     process.exit(2);
   });
 
